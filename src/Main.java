6de2ce05fda359502ec5a8cd2220a73ffc22e022
wulft <Tom.Wulf@uc.edu>
2022-10-27 10:13:57 -0400

    Completed example of using a sparse array for input Cas Register shows the sub prices as well as the totoal and average

--- src/Main.java
+++ src/Main.java
@@ -29,11 +29,35 @@ public class Main
            items[recCount] = price;
            recCount++;
 
-           done = SafeInput.getYNConfirm(console, "Are you done");
-
+           // Prevent an overload of input
+           if(recCount == 99)
+           {
+               System.out.println("Out of Memory, reached the max num of records!");
+               System.out.println("Don\'t you have a life!?");
+               done = true;
+           }
+           else
+           {
+               done = SafeInput.getYNConfirm(console, "Are you done");
+           }
        }while(!done);
 
+       ave = total / recCount;
+
        // Now code to display the list of item prices and the total in a table using printf formatting
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("****                      Shopping Recept                               ***");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        for(int r = 0; r < recCount; r++)
+        {
+            System.out.printf("   Item:%3d%63.2f\n",r+1, items[r]);
+        }
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.printf("   Total: %65.2f\n", total);
+        System.out.printf("   Ave: %67.2f", ave);
 
     }
 }
